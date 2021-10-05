a1a37a1e54d0e9dec5aac236c8d637e9a1a22867
Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>
2021-10-05 19:46:34 -0300

    Mejora de legibilidad
    Signed-off-by: Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>

--- Trabajo3Ingles.c
+++ Trabajo3Ingles.c
@@ -1,181 +1,180 @@
-/*Cree un programa utilizando listas enlazadas con puntero a cabeza y cola que contenga las
-siguientes funciones:
-- Eliminar todos los nodos con un valor dado por el usuario.
-- Diseña una función que devuelva un “corte” de la lista. Se recibirán dos índices i y j y se
-devolverá una nueva lista con punteros a cabeza y cola con una copia de los nodos que
-van del que ocupa la posición i al que ocupa la posición j − 1, ambos incluidos. La lista
-devuelta tendrá punteros a cabeza y cola.*/
+/********************************************************************************************
+ Cree un programa utilizando listas enlazadas con puntero a cabeza y cola que contenga las
+  siguientes funciones:
+  - Eliminar todos los nodos con un valor dado por el usuario.
+  - Diseña una función que devuelva un “corte” de la lista. Se recibirán dos índices i y j y se
+  devolverá una nueva lista con punteros a cabeza y cola con una copia de los nodos que
+  van del que ocupa la posición i al que ocupa la posición j − 1, ambos incluidos. La lista
+  devuelta tendrá punteros a cabeza y cola
+**********************************************************************************************/
 
 #include <stdio.h>
 #include <stdlib.h>
 
-struct List
+typedef struct struct_node
 {
-    int num;
-    struct List *next;
-};
+	int num;
+	struct struct_node *next;
+} node;
 
-typedef struct Node
+typedef struct
 {
-    struct List *head;
-    struct List *tail;
-    int  longitude;
-}SNode;
+	node *head;
+	node *tail;
+	int  longitude;
+} st_lista;
 
-int Menu();
-SNode EnterHead(SNode list);
-SNode EnterTail(SNode list);
-void ShowList(SNode List);
-void DeleteItem(SNode list,int n);
+int menu();
+st_lista ingresar_cabeza(st_lista list);
+st_lista ingresar_cola(st_lista list);
+void mostrar_lista(st_lista list);
+void borrar_item_por_numero(st_lista list,int n);
 
 int main(void)
 {
-    int option;
+	int option, n;
 
-    SNode list;
-    
-    list.head=list.tail=NULL;
+	st_lista list;
 
-    do
-    {
-        option=Menu();
+	list.head=list.tail=NULL;
 
-        switch (option)
-        {
-        case 1:
-            list=EnterHead(list);
-            break;
-        case 2:
-            ShowList(list);
-            break;
-        case 3:
-            list=EnterTail(list);
-            break;
-        case 4:
-            int n;
-            printf("index of the number to be deleted: ");
-            scanf("%d",&n);
-            DeleteItem(list,n);
-        case 5:
-            printf("Thanks for using app");
-            break;
-        default:
-            puts("Option Incorrect");
-            break;
-        }
-    } while (option!=5);
-    
+	do
+	{
+		option=menu();
 
-    return 0;
+		switch (option)
+		{
+		case 1:
+			list=ingresar_cabeza(list);
+			break;
+		case 2:
+			mostrar_lista(list);
+			break;
+		case 3:
+			list=ingresar_cola(list);
+			break;
+		case 4:
+			printf("index of the number to be deleted: ");
+			scanf("%d",&n);
+			borrar_item_por_numero(list,n);
+		case 5:
+			printf("Thanks for using app");
+			break;
+		default:
+			puts("Option Incorrect");
+			break;
+		}
+	} while (option!=5);
+
+
+	return 0;
 }
 
-void DeleteNode(List* node)
+void borrar_nodo(node* node)
 {
-    free(node);
+	free(node);
 }
 
-int Count(SNode* list)
+int contar(st_lista* list)
 {
-    return list->longitude;
+	return list->longitude;
 }
 
-SNode EnterHead(SNode list)
+st_lista ingresar_cabeza(st_lista list)
 {
-    struct List *new_Item=(List *)malloc(sizeof(List));
+	node *nuevo_item=(node *)malloc(sizeof(node));
 
-    if (new_Item!=NULL)
-    {
-        puts("Enter a number: ");
-        scanf("%d",&new_Item->num);
-        new_Item->next=NULL;
-    }
-    if (list.head==NULL)
-    {
-        list.head=list.tail=new_Item;
-    }
-    else
-    {
-        new_Item->next=list.head;
-        list.head=new_Item;
-    }
-    list.longitude++;
+	if (nuevo_item!=NULL)
+	{
+		puts("Enter a number: ");
+		scanf("%d",&nuevo_item->num);
+		nuevo_item->next=NULL;
+	}
+	if (list.head==NULL)
+	{
+		list.head=list.tail=nuevo_item;
+	}
+	else
+	{
+		nuevo_item->next=list.head;
+		list.head=nuevo_item;
+	}
+	list.longitude++;
 
-    return list;
+	return list;
 }
 
-SNode EnterTail(SNode list)
+st_lista ingresar_cola(st_lista list)
 {
-    struct List *new_Item=(List *)malloc(sizeof(List));
+	node *nuevo_item=(node *)malloc(sizeof(node));
 
-    if (new_Item!=NULL)
-    {
-        puts("Enter a number: ");
-        scanf("%d",&new_Item->num);
-        new_Item->next=NULL;
-    }
-    if (list.head==NULL)
-    {
-        list.head=list.tail=new_Item;
-    }
-    else
-    {
-        list.tail->next=new_Item;
-        list.tail=new_Item;
-    }
-    list.longitude++;
+	if (nuevo_item!=NULL)
+	{
+		puts("Enter a number: ");
+		scanf("%d",&nuevo_item->num);
+		nuevo_item->next=NULL;
+	}
+	if (list.head==NULL)
+	{
+		list.head=list.tail=nuevo_item;
+	}
+	else
+	{
+		list.tail->next=nuevo_item;
+		list.tail=nuevo_item;
+	}
+	list.longitude++;
 
-    return list;
+	return list;
 }
 
-void ShowList(SNode list)
+void mostrar_lista(st_lista list)
 {
-    List *temp;
+	node *temp;
 
-    for ( temp = list.head; temp != NULL; temp = temp->next)
-    {
-        printf("%d\t",temp->num);
-    }
+	for ( temp = list.head; temp != NULL; temp = temp->next)
+	{
+		printf("%d\t",temp->num);
+	}
 }
 
-void DeleteItem(SNode list, int n)
+void borrar_item_por_numero(st_lista list, int n)
 {
-    List *temp;
-
-    if (list.head)
-    {
-        if(n==0)
-        {
-            List *temp1=list.head;
-            list.head=list.head->next;
-            DeleteNode(temp1);
-        }
-        else if (n<list.longitude)
-        {
-            List *temp2=list.head;
-            int index=0;
-            while (index<(n-1))
-            {
-                temp2=temp2->next;
-                index++;
-            }
-            List *deleteItem=temp2->next;
-            temp2->next=deleteItem->next;
-            DeleteNode(deleteItem);
-            list.longitude--;
-        }
-    }
+	if (list.head)
+	{
+		if(n==0)
+		{
+			node *temp1=list.head;
+			list.head=list.head->next;
+			borrar_nodo(temp1);
+		}
+		else if (n<list.longitude)
+		{
+			node *temp2=list.head;
+			int index=0;
+			while (index<(n-1))
+			{
+				temp2=temp2->next;
+				index++;
+			}
+			node *deleteItem=temp2->next;
+			temp2->next=deleteItem->next;
+			borrar_nodo(deleteItem);
+			list.longitude--;
+		}
+	}
 }
 
-int Menu()
+int menu()
 {
-    int option;
-    puts("");
-    puts("1- Enter head");
-    puts("2- Show Node");
-    puts("3- Enter tail");
-    puts("4- Delete Item");
-    puts("5- Get Out");
-    scanf("%d",&option);
+	int option;
+	puts("");
+	puts("1- Enter head");
+	puts("2- Show list");
+	puts("3- Enter tail");
+	puts("4- Delete Item");
+	puts("5- Get Out");
+	scanf("%d",&option);
 
-    return option;
+	return option;
 }
