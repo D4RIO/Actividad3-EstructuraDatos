1fe773eff90b1678f7f9aa4eea1f5356e152359b
Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>
2021-10-05 21:32:05 -0300

    Varias correcciones
    Dejé comentarios en el código. Había procedimientos que modificaban la lista
    pero no la recibían por referencia, causando que modifiquen una
    copia del handler y se pierdan las modificaciones.
    
    Además, no inicializabas una variable antes de su uso, cosa que puede causar
    problemas de seguridad en aplicaciones de la vida real.
    
    Básicamente, este commit pone a funcionar lo que estaba hecho.
    
    Signed-off-by: Rodríguez Poggio, Darío Andrés <rodriguez.dario.a@gmail.com>

--- Trabajo3Ingles.c
+++ Trabajo3Ingles.c
@@ -11,6 +11,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// esta es la estructura de cada nodo
 typedef struct struct_node
 {
 	int num;
@@ -18,6 +19,7 @@ typedef struct struct_node
 
 } node;
 
+// esta estructura es solo para un handler de la lista
 typedef struct
 {
 	node *head;
@@ -27,10 +29,10 @@ typedef struct
 } st_lista;
 
 int menu ();
-st_lista ingresar_cabeza (st_lista list);
-st_lista ingresar_cola (st_lista list);
+void ingresar_cabeza (st_lista *list); // procedimiento que modifica la lista
+void ingresar_cola (st_lista *list); // procedimiento que modifica la lista
 void mostrar_lista (st_lista list);
-void borrar_item_por_numero (st_lista list, int n);
+void borrar_item_por_numero (st_lista *list, int n); // procedimiento que modifica la lista
 
 int main (void)
 {
@@ -39,6 +41,7 @@ int main (void)
 	st_lista list;
 
 	list.head = list.tail = NULL;
+	list.longitude = 0; // no estabas inicializando (!)
 
 	do
 	{
@@ -47,18 +50,19 @@ int main (void)
 		switch (option)
 		{
 		case 1:
-			list = ingresar_cabeza (list);
+			ingresar_cabeza (&list);
 			break;
 		case 2:
 			mostrar_lista (list);
 			break;
 		case 3:
-			list = ingresar_cola (list);
+			ingresar_cola (&list);
 			break;
 		case 4:
 			printf ("index of the number to be deleted: ");
 			scanf ("%d", &n);
-			borrar_item_por_numero (list, n);
+			borrar_item_por_numero (&list, n);
+			break;
 		case 5:
 			printf ("Thanks for using app");
 			break;
@@ -82,7 +86,7 @@ int contar (st_lista* list)
 	return list->longitude;
 }
 
-st_lista ingresar_cabeza (st_lista list)
+void ingresar_cabeza (st_lista *list)
 {
 	node *nuevo_item = (node *) malloc (sizeof (node));
 
@@ -92,22 +96,20 @@ st_lista ingresar_cabeza (st_lista list)
 		scanf("%d", &nuevo_item->num);
 		nuevo_item->next = NULL;
 	}
-	if (! list.head)
+	if (! list->head)
 	{
-		list.head = list.tail = nuevo_item;
+		list->head = list->tail = nuevo_item;
 	}
 	else
 	{
-		nuevo_item->next = list.head;
-		list.head = nuevo_item;
+		nuevo_item->next = list->head;
+		list->head = nuevo_item;
 	}
 
-	list.longitude++;
-
-	return list;
+	list->longitude++;
 }
 
-st_lista ingresar_cola (st_lista list)
+void ingresar_cola (st_lista *list)
 {
 	node *nuevo_item = (node *) malloc (sizeof (node));
 
@@ -117,19 +119,17 @@ st_lista ingresar_cola (st_lista list)
 		scanf("%d", &nuevo_item->num);
 		nuevo_item->next = NULL;
 	}
-	if (! list.head)
+	if (! list->head)
 	{
-		list.head = list.tail = nuevo_item;
+		list->head = list->tail = nuevo_item;
 	}
 	else
 	{
-		list.tail->next = nuevo_item;
-		list.tail = nuevo_item;
+		list->tail->next = nuevo_item;
+		list->tail = nuevo_item;
 	}
 
-	list.longitude++;
-
-	return list;
+	list->longitude++;
 }
 
 void mostrar_lista (st_lista list)
@@ -142,29 +142,33 @@ void mostrar_lista (st_lista list)
 	}
 }
 
-void borrar_item_por_numero(st_lista list, int n)
+void borrar_item_por_numero(st_lista *list, int n)
 {
-	if (list.head)
+	if (list->head)
 	{
-		if(n == 0)
+		// nadie va a borrar el elemento 0, lo normal
+		// es pensarlo que el elemento 1 es el 1ro
+		if(n == 1)
 		{
-			node *temp1 = list.head;
-			list.head = list.head->next;
+			node *temp1 = list->head;
+			list->head = list->head->next;
 			borrar_nodo (temp1);
 		}
-		else if (n < list.longitude)
+		// n hasta la longitud de la lista, sino no podés borrar el último
+		else if (n <= list->longitude)
 		{
-			node *temp2 = list.head;
-			int index = 0;
-			while (index < (n-1))
-			{
+			node *temp2 = list->head;
+
+			// el 'while' implementado acá era un 'for'
+			// y debía ir desde 1 a (n-1), o un intervalo equivalente
+			// (el intervalo era muy grande)
+			for (int i = 1; i < (n-1); i++)
 				temp2 = temp2->next;
-				index++;
-			}
+
 			node *deleteItem = temp2->next;
 			temp2->next = deleteItem->next;
 			borrar_nodo (deleteItem);
-			list.longitude--;
+			list->longitude--;
 		}
 	}
 }
